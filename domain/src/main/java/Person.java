6a37c265984c154826dbbc2d9d2c8794d905e814
MRedant <mredant@gmail.com>
2018-03-12 09:01:37 +0100

    f******* '-' missing X-)

--- domain/src/main/java/Person.java
+++ domain/src/main/java/Person.java
@@ -5,6 +5,10 @@ public class Person {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public String toString() {
         return "Person name= " + name ;
